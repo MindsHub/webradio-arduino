cc6a941e82b5f85412b7761407c3cb1404762690
Stypox <stypox@pm.me>
2023-08-26 22:29:58 +0200

    Allow custom signatures in create_cpp.py

--- include/static_resources.hpp
+++ include/static_resources.hpp
@@ -2,6 +2,6 @@
 #ifndef _HTML_HPP_
 #define _HTML_HPP_
 
-extern const char * homePageHtml __attribute__((static_file("home_page.html")));
+extern const char* homePageHtml __attribute__((static_file("home_page.html")));
 
 #endif // _HTML_HPP_
